92696e0d8eaa11b3eeaa2229180541d31e6dcf1a
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-09-17 21:16:36 -0300

    upgrade project

--- database/api.js
+++ database/api.js
@@ -1,16 +1,17 @@
-async function fetchProducts() {
-    try {
-        const response = await fetch('https://api-rhmw.onrender.com/').then(response => response.json());
 
-        console.log(response);
 
-    } catch (error) {
+fetch('https://api-rhmw.onrender.com/')
+    .then(response => response.json())
+    .then(data => {
+        
+        console.log(data);
+
+        // let nome = document.querySelector('.nome');
+        // let imagem = document.querySelector('.imagem');
+        // nome.innerHTML = data[3].DESCRICAO;
+        // imagem.src = data[3].IMAGEM;
+     
+    })
+    .catch(error => {
         console.error("Erro ao buscar os dados:", error);
-    }
-}
-
-// Chamando a função
-fetchProducts();
-
-
-// const dados = await fetch
+    });
