666b55c2880fe38f41e0f20d13d514bc67cc9c71
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-09-25 23:35:37 -0300

    upgrade

--- javascripts/login.js
+++ javascripts/login.js
@@ -7,11 +7,11 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     fetch('keys.json')
         .then(response => response.json())
         .then(data => {
-            
+
             const validUsername = data.user;
             const validPassword = data.password;
 
-           
+
             if (username === validUsername && password === validPassword) {
                 window.location.href = './login-sucess.html';
             } else {
@@ -21,3 +21,35 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         .catch(error => console.error('Erro ao carregar o JSON:', error));
 });
 
+
+
+// FUNCAO PEGAR DADOS
+
+// const url = 'https://script.google.com/macros/s/AKfycbw495fveSWtSytsZ8Hi9wLXeEX69-L78GgtniGMzAIw6FoTAIMGTmmCMzdOXC7dFZAJxA/exec';
+// const formulario = document.forms['loginForm'];
+
+// // const url = 'https://jsonplaceholder.typicode.com/posts';
+
+// formulario.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     fetch(url, { 
+//         method: 'POST', 
+//         mode: 'no-cors',
+//         body: new FormData(formulario) 
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Erro na resposta da rede');
+//         }
+//         return response.json(); 
+//     })
+//     .then(data => {
+//         alert('Dados enviados com sucesso: ' + JSON.stringify(data));
+//         formulario.reset();
+//     })
+//     .catch(error => {
+//         console.error('Erro no envio dos dados', error);
+//         alert('Erro ao enviar os dados: ' + error.message);
+//     });
+// });
