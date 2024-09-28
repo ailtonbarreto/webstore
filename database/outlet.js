81f29044a78d83ec8829237ce8c82b8af61babd7
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-09-28 05:48:23 -0300

    upgrade

--- database/outlet.js
+++ database/outlet.js
@@ -37,7 +37,7 @@ fetch(url)
             // imageLink.href = "./produto.html";
 
             let imagem = document.createElement("img");
-            imagem.addEventListener("click", produtoclicado1);
+            imagem.addEventListener("click", produtoclicado);
             imagem.src = item.IMAGEM;
             imagem.alt = item.DESCRICAO;
             imageLink.appendChild(imagem);
@@ -73,44 +73,5 @@ fetch(url)
     })
     .catch(error => {
         console.error("Erro ao buscar os dados:", error);
-});
-
-function produtoclicado1(event) {
-
-    let selected_product = event.target;
-
-
-    let elementoPai = selected_product.parentElement.parentElement;
-
-
-    let foto = elementoPai.querySelector("img").src;
-    let produtonome = elementoPai.querySelector("img").alt;
-    let precoDe = elementoPai.querySelector(".preco_de").textContent;
-    let precoPor = elementoPai.querySelector(".preco_por").textContent;
-
-    console.log("Clicou no produto");
-    console.log("parent do produto", elementoPai.id);
-    console.log("foto do produto:", foto);
-    console.log("nome: ",produtonome);
-    console.log("Preço de:",precoDe);
-    console.log("Preço por:",precoPor);
-
-    // Armazena no localStorage o id do elemento pai
-    localStorage.setItem("produtoSelecionado", elementoPai.id);
-    localStorage.setItem("foto",foto);
-    localStorage.setItem("nome",produtonome);
-    localStorage.setItem("preco_de",precoDe);
-    localStorage.setItem("preco_por",precoPor);
-
-    // Redireciona para outra página, se necessário
-    window.location.href = "./produto.html";
-}
-
-// Seleciona todos os elementos que possuem a classe 'produto' (por exemplo)
-let produtos = document.querySelectorAll(".produto");
-
-// Adiciona o evento de clique para todos os produtos
-produtos.forEach(produto => {
-    produto.addEventListener("click", produtoclicado);
 });
 
