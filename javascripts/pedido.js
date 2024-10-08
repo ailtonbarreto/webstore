a460773863720641fe014cfa821128e96cc3410f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-10-07 23:37:02 -0300

    upgrade

---
+++ javascripts/pedido.js
@@ -0,0 +1,7 @@
+
+
+// Tenta pegar o carrinho do localStorage
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+console.log("Carrinho do localStorage:", cart);  // Verifica o conteúdo do carrinho
+
+
