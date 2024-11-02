27cd609b2cdea4747d95a167b19e08da4e5dfb36
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-11-02 19:44:03 -0300

    updated

--- javascripts/login.js
+++ javascripts/login.js
@@ -3,25 +3,34 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    let logged = 0;
+    let logged = "0";
 
     fetch('./database/keys.json')
         .then(response => response.json())
-        .then(data => {
-            const validUsername = data.user;
-            const validPassword = data.password;
+        .then(config => {
+            const apiUrl = config.tb_cliente;
 
-            if (username === validUsername && password === validPassword) {
-                window.location.href = './index.html';
+            return fetch(apiUrl);
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            const user = data.find(user => 
+                user.USER === username && 
+                user.PASSWORD === password &&
+                user.STATUS === 1
+            );
+
+            if (user) {
+                // Login bem-sucedido
                 logged = "1";
+                window.location.href = './index.html';
             } else {
-            
-                // window.location.href = './index.html';
-                logged = "0";
-                alert("Usuário ou Senha incorretos!")
+                // Login falhou (usuário/senha incorretos ou status inativo)
+                alert("Usuário ou Senha incorretos ou conta inativa!");
             }
 
             localStorage.setItem("logged", logged);
         })
-        .catch(error => console.error('Erro ao carregar o JSON:', error));
+        .catch(error => console.error('Erro ao carregar os dados:', error));
 });
