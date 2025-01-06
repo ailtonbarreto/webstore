f55491d61d11fbb288381ebc567f43c691e7bd08
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-01-06 18:26:30 -0300

    updated

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,5 +1,13 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+// Seleciona o formulário e o modal de carregamento
+const loginForm = document.getElementById("loginForm");
+const loadingModal = document.getElementById("loading-modal");
+
+// Adiciona um evento ao enviar o formulário
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio automático do formulário
+
+    // Exibe o modal de carregamento com o spinner
+    loadingModal.style.display = "flex";
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -9,12 +17,10 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         .then(response => response.json())
         .then(config => {
             const apiUrl = config.tb_cliente;
-
             return fetch(apiUrl);
         })
         .then(response => response.json())
         .then(data => {
-
             const user = data.find(user => 
                 user.USER === username && 
                 user.PASSWORD === password &&
@@ -22,17 +28,21 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
             );
 
             if (user) {
-             
                 logged = "1";
+                localStorage.setItem("sku_cliente", user.SKU_CLIENTE);
                 window.location.href = './index.html';
-                sku_cliente = localStorage.setItem("sku_cliente", user.SKU_CLIENTE);
             } else {
-         
                 alert("Usuário ou Senha incorretos ou conta inativa!");
-     
             }
 
             localStorage.setItem("logged", logged);
         })
-        .catch(error => console.error('Erro ao carregar os dados:', error));
+        .catch(error => {
+            console.error('Erro ao carregar os dados:', error);
+            alert("Ocorreu um erro ao tentar realizar o login. Tente novamente mais tarde.");
+        })
+        .finally(() => {
+            // Oculta o modal de carregamento ao final, independente do sucesso ou erro
+            loadingModal.style.display = "none";
+        });
 });
